fe185ef14631b5d631005bc10ba58842c1b32243
DALILA <dalila7300@gmail.com>
2024-10-20 12:21:56 -0500

    chore: initial commit

---
+++ src/main/java/com/example/sabores_cercanos/SaboresCercanosApplication.java
@@ -0,0 +1,13 @@
+package com.example.sabores_cercanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaboresCercanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaboresCercanosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sabores_cercanos/SaboresCercanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sabores_cercanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaboresCercanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
